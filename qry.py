b6d321e934d49a58e4eab39da2d418502b592aec
w1ll1ey <vili.ilari.kaisto@gmail.com>
2024-04-12 19:35:15 +0300

    App refactored and added creating comparisons

---
+++ qry.py
@@ -0,0 +1,73 @@
+from db import db
+from sqlalchemy.sql import text
+
+
+def get_userdata(email):
+    sql = text("SELECT id, password FROM users WHERE email=:email")
+    result = db.session.execute(sql, {"email":email})
+    user = result.fetchone()
+    return user
+
+def add_userdata(email, hash_value):
+    sql = text("INSERT INTO users (email, password) VALUES (:email, :password)")
+    db.session.execute(sql, {"email":email, "password":hash_value})
+    db.session.commit()
+
+def add_car(manufacturer, model, gen, type):
+    sql = "INSERT INTO cars (manufacturer, model, generation, type) VALUES (:manufacturer, :model, :generation, :type)"
+    db.session.execute(text(sql), {"manufacturer":manufacturer, "model":model, "generation":gen, "type":type})
+    db.session.commit()
+
+def get_user_id(email):
+    sql = text("SELECT id FROM users WHERE email=:email")
+    result = db.session.execute(sql, {"email":email})
+    extract = result.fetchone()
+    user_id = str(extract).replace("(", "").replace(")", "").replace(",", "")
+    return user_id
+
+def add_comparison(name, userid):
+    sql = text("INSERT INTO comparisons (name, userid) VALUES (:name, :userid)")
+    db.session.execute(sql, {"name":name, "userid":userid})
+    db.session.commit()
+
+def get_comparisonid(name):
+    sql = text("SELECT id FROM comparisons WHERE name=:name")
+    result = db.session.execute(sql, {"name":name})
+    extract = result.fetchone()
+    comparison_id = str(extract).replace("(", "").replace(")", "").replace(",", "")
+    return comparison_id
+
+def get_carid(manufacturer, model, gen, type):
+    sql = text("SELECT id FROM cars WHERE manufacturer=:manufacturer AND model=:model AND generation=:generation AND type=:type")
+    result = db.session.execute(sql, {"manufacturer":manufacturer, "model":model, "generation":gen, "type":type})
+    extract = result.fetchone()
+    car_id = str(extract).replace("(", "").replace(")", "").replace(",", "")
+    return car_id
+
+
+def update_comparison(comparisonid, carid):
+    sql = text("INSERT INTO comparisoncars (comparisonid, carid) VALUES (:comparisonid, :carid)")
+    db.session.execute(sql, {"comparisonid":comparisonid, "carid":carid})
+    db.session.commit()
+
+def get_comparison_data(userid):
+    sql = text("SELECT name, id FROM comparisons WHERE userid=:userid")
+    result = db.session.execute(sql, {"userid":userid})
+    extract = result.fetchall()
+    return extract
+
+def get_comparison_cars(comparisonid):
+    sql = text("SELECT carid FROM comparisoncars WHERE comparisonid=:comparisonid")
+    result = db.session.execute(sql, {"comparisonid":comparisonid})
+    extract = result.fetchall()
+    carids = []
+    for value in extract:
+        id = str(value).replace("(", "").replace(")", "").replace(",", "")
+        carids.append(id)
+    return carids
+
+def get_car_data(carid):
+    sql = text("SELECT (manufacturer, model, generation, type) FROM cars WHERE id=:id")
+    result = db.session.execute(sql, {"id":carid})
+    extract = result.fetchall
+    return extract
---
+++ routes.py
@@ -0,0 +1,102 @@
+from app import app
+import qry
+import json
+from flask import redirect, render_template, request, session, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
+
+@app.route("/")
+def index():
+    email = session["email"]
+    user_id = qry.get_user_id(email)
+    comparisons = qry.get_comparison_data(user_id)
+    return render_template("index.html", comparisons = comparisons)
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+    if not qry.get_userdata(email):
+        return "Sähköpostiosoitteeseen ei ole liitetty tiliä!"
+    else:
+        hash_value = qry.get_userdata(email).password
+        if check_password_hash(hash_value, password):
+            session["email"] = email
+            return redirect("/")
+        else:
+            return "Väärä salasana!"
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form["email"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    qry.add_userdata(email, hash_value)
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["email"]
+    return redirect("/")
+
+@app.route("/createcar")
+def createcar():
+    return render_template("createcar.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    manufacturer = request.form["manufacturer"]
+    model = request.form["model"]
+    gen = request.form["generation"]
+    type = request.form["type"]
+    qry.add_car(manufacturer, model, gen, type)
+    return redirect("/createcar")
+
+#@app.route("/result", methods=["POST"])
+#def result():
+#    return render_template("createcarresult.html", manufacturer=request.form["manufacturer"], model=request.form["model"], type=request.form["type"])
+
+@app.route("/createcomparison", methods=["POST"])
+def createcomparison():
+    email = session["email"]
+    name = request.form["name"]
+    userid = qry.get_user_id(email)
+    qry.add_comparison(name, userid)
+    value = qry.get_comparisonid(name)
+    session["comparisonid"] = value
+    return redirect("/editcomparison")
+
+@app.route("/editcomparison")
+def editcomparison():
+    return render_template("selection.html")
+
+@app.route("/addcar", methods=["POST"])
+def addcar():
+    manufacturer = request.form["manufacturer"]
+    model = request.form["model"]
+    gen = request.form["generation"]
+    type = request.form["type"]
+    carid = qry.get_carid(manufacturer, model, gen, type)
+    comparisonid = session["comparisonid"]
+    qry.update_comparison(comparisonid, carid)
+    return redirect("/editcomparison")
+
+@app.route("/comparison", methods=["POST"])
+def comparison():
+    comparison = request.form["comparison"]
+    carids = qry.get_comparison_cars(comparison)
+    cardata = qry.get_car_data(carids[0])
+    print(str(cardata))
+    return render_template("comparison.html")
+
+
+
+
+
+
+@app.route("/registered", methods=["POST"])
+def registered():
+    return "Vahvistussähköposti lähetetty osoitteeseen " + request.form["email"] + "."
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html", username=request.form["username"])
